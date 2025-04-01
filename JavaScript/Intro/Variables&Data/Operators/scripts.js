3cffa8c8e47381abe37c8476bca2cb8e55f078b7
Colby <maquiret@gmail.com>
2025-03-31 20:40:54 -0500

    finished first module

---
+++ JavaScript/Intro/Variables&Data/Operators/scripts.js
@@ -0,0 +1,14 @@
+const x = 10;
+const y = 5;
+
+console.log(x + y); //15
+console.log(x - y); //5
+console.log(x * y); //50
+console.log(x / y); //2
+console.log(x & y); //0 it is the remainder if you x = 11 then it would be 1
+
+// comparison Ops
+console.log(x > y); //true
+console.log(x === 10)  //False (strict operator)
+// console.log(x == 10)  //true (not strict operator)
+console.log(x !== y); //true (!== is a not equal to)
